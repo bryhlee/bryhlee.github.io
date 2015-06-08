af35fd8f7095e82f06f2183ee2508c4d41ebabb0
bryhlee <bhlee@scu.edu>
2015-06-07 20:04:06 -0700

    Update

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,11 @@
 new WOW().init();
 
 /* Smooth Scroll Links */
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+(function() {
+  ('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : ('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
@@ -41,4 +41,4 @@ $(document).ready(function() {
 /* Preloader */
 $(window).load( setTimeout( function() {
 	$(".loader").fadeOut("slow");
-}, 400)); 
+}, 300)); 
